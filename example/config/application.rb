cb00cbaeceb6ce41e65b9bafee1b74d33d9a9a2a
Justin Searls <searls@gmail.com>
2016-02-02 16:27:15 -0500

    Make compatible with Rails 4.1

--- example/config/application.rb
+++ example/config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Example
   class Application < Rails::Application
+    if config.respond_to?("eager_load=")
+      config.eager_load = false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
