debf694dbfdf5dbe72840cee5e1f2d9db3851f03
Justin Searls <searls@gmail.com>
2021-06-30 09:11:00 -0400

    update deps, get back to passing locally

--- example/db/migrate/20160202163803_change_pants.rb
+++ example/db/migrate/20160202163803_change_pants.rb
@@ -1,6 +1,7 @@
 class ChangePants < ActiveRecord::Migration[4.2]
   class Pant < ActiveRecord::Base
   end
+
   def change
     Pant.all.each do |pant|
       # do a migration depending on the redefined Pant model
