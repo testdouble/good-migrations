9434a6af04533ae1e46a9baf34d258f43fa0e3b1
Maxime Lapointe <hunter_spawn@hotmail.com>
2021-11-15 20:34:00 -0500

    Make configuration a singleton PORO

--- lib/good_migrations/configuration.rb
+++ lib/good_migrations/configuration.rb
@@ -1,29 +1,34 @@
 module GoodMigrations
+  def self.config
+    @configuration ||= Configuration.new
+  end
+
   class Configuration
-    class << self
-      # Migrations with timestamps (the numbers at the beginning of the file name) from
-      # before this configured time will be allowed to perform autoloading, bypassing the
-      # mechanism of this gem. Accepts:
-      #   nil: meaning never permit it
-      #   String accepted by `Time.parse`, such as: 20211103150610 or 20211103_150610
-      #   object responding to `to_time`, such as Date and Time
-      def permit_autoloading_before_date=(value)
-        case value
-        when nil
-          # Stay nil
-        when String
-          value = Time.parse(value)
+    # Migrations with timestamps (the numbers at the beginning of the file name) from
+    # before this configured time will be allowed to perform autoloading, bypassing the
+    # mechanism of this gem. Accepts:
+    #   nil (default): never permit autoload
+    #   String accepted by `Time.parse`, such as: 20211103150610 or 20211103_150610
+    #   object responding to `to_time`, such as Date and Time
+    attr_reader :permit_autoloading_before_date
+    def permit_autoloading_before_date=(value)
+      case value
+      when nil
+        # Stay nil
+      when String
+        value = Time.parse(value)
+      else
+        if value.respond_to?(:to_time)
+          value = value.to_time
         else
-          if value.respond_to?(:to_time)
-            value = value.to_time
-          else
-            raise "Received an invalid value for permit_autoloading_before_date: #{value.inspect}"
-          end
+          raise "Received an invalid value for permit_autoloading_before_date: #{value.inspect}"
         end
-        @permit_autoloading_before_date = value
       end
-      attr_reader :permit_autoloading_before_date
-      Configuration.permit_autoloading_before_date = nil
+      @permit_autoloading_before_date = value
+    end
+
+    def initialize
+      @permit_autoloading_before_date = nil
     end
   end
 end
--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -1,41 +1,45 @@
 require "test_helper"
 
 class ConfigurationTest < Minitest::Test
-  def test_permit_autoloading_before_date_set_nil
-    GoodMigrations::Configuration.permit_autoloading_before_date = nil
+  def setup
+    @config = GoodMigrations::Configuration.new
+  end
 
-    assert_nil GoodMigrations::Configuration.permit_autoloading_before_date
+  def test_permit_autoloading_before_date_set_nil
+    @config.permit_autoloading_before_date = nil
+
+    assert_nil @config.permit_autoloading_before_date
   end
 
   def test_permit_autoloading_before_date_set_string
-    GoodMigrations::Configuration.permit_autoloading_before_date = "20210203"
-    assert_equal Time.new(2021, 2, 3), GoodMigrations::Configuration.permit_autoloading_before_date
+    @config.permit_autoloading_before_date = "20210203"
+    assert_equal Time.new(2021, 2, 3), @config.permit_autoloading_before_date
 
-    GoodMigrations::Configuration.permit_autoloading_before_date = "20210203101112"
-    assert_equal Time.new(2021, 2, 3, 10, 11, 12), GoodMigrations::Configuration.permit_autoloading_before_date
+    @config.permit_autoloading_before_date = "20210203101112"
+    assert_equal Time.new(2021, 2, 3, 10, 11, 12), @config.permit_autoloading_before_date
 
-    GoodMigrations::Configuration.permit_autoloading_before_date = "20210203_121112"
-    assert_equal Time.new(2021, 2, 3, 12, 11, 12), GoodMigrations::Configuration.permit_autoloading_before_date
+    @config.permit_autoloading_before_date = "20210203_121112"
+    assert_equal Time.new(2021, 2, 3, 12, 11, 12), @config.permit_autoloading_before_date
 
-    GoodMigrations::Configuration.permit_autoloading_before_date = "2021-04-03 12:11:12"
-    assert_equal Time.new(2021, 4, 3, 12, 11, 12), GoodMigrations::Configuration.permit_autoloading_before_date
+    @config.permit_autoloading_before_date = "2021-04-03 12:11:12"
+    assert_equal Time.new(2021, 4, 3, 12, 11, 12), @config.permit_autoloading_before_date
   end
 
   def test_permit_autoloading_before_date_set_bad_string
     assert_raises(ArgumentError) do
-      GoodMigrations::Configuration.permit_autoloading_before_date = "abc"
+      @config.permit_autoloading_before_date = "abc"
     end
   end
 
   def test_permit_autoloading_before_date_set_date
-    GoodMigrations::Configuration.permit_autoloading_before_date = Date.new(2021, 4, 15)
+    @config.permit_autoloading_before_date = Date.new(2021, 4, 15)
 
-    assert_equal Time.new(2021, 4, 15), GoodMigrations::Configuration.permit_autoloading_before_date
+    assert_equal Time.new(2021, 4, 15), @config.permit_autoloading_before_date
   end
 
   def test_permit_autoloading_before_date_set_time
-    GoodMigrations::Configuration.permit_autoloading_before_date = Time.new(2021, 4, 20, 16, 17, 18)
+    @config.permit_autoloading_before_date = Time.new(2021, 4, 20, 16, 17, 18)
 
-    assert_equal Time.new(2021, 4, 20, 16, 17, 18), GoodMigrations::Configuration.permit_autoloading_before_date
+    assert_equal Time.new(2021, 4, 20, 16, 17, 18), @config.permit_autoloading_before_date
   end
 end
